7e7b12112302a39146f39f728983500839f47e42
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-03-01 14:04:09 -0500

    ADD: Finished Restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -31,14 +31,39 @@ const restaurants = [
       "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1c/36/67/bd/20201018-144215-largejpg.jpg?w=1000&h=-1&s=1",
   },
   {
-    name: "The Sushi Place",
-    description: "Fresh and tasty sushi!",
-    image: "/steak.jpg",
+    name: "Dobbin House Tavern",
+    description:
+      "A colonial restaurant offering candlelit elegance, abundant food, and gracious service that evoke the charm of two centuries ago.",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/fb/df/8a/photo0jpg.jpg?w=1400&h=-1&s=1",
   },
   {
-    name: "The Sushi Place",
-    description: "Fresh and tasty sushi!",
-    image: "/steak.jpg",
+    name: "Farnsworth House Tavern",
+    description:
+      "The Farnsworth House Inn is a bed and breakfast and tourist attraction located in Gettysburg, Pennsylvania. ",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/09/55/b0/0d/farnsworth-house-tavern.jpg?w=900&h=-1&s=1",
+  },
+  {
+    name: "1863 Restaurant",
+    description:
+      "Casual bistro in the Wyndham Hotel offering a menu of American eats, from omelets to steak.",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/c3/d9/65/1863-restaurant-at-the.jpg?w=800&h=-1&s=1",
+  },
+  {
+    name: "Appalachian Brewing Company",
+    description:
+      "Open, airy brewery serves housemade beers & craft sodas alongside burgers & other casual bites.",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/38/1a/51/photo1jpg.jpg?w=2000&h=-1&s=1",
+  },
+  {
+    name: "Blue & Gray Bar & Grill",
+    description:
+      "Casual bistro in the Wyndham Hotel offering a menu of American eats, from omelets to steak.",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/12/72/59/47/20180326-132421-largejpg.jpg?w=1000&h=-1&s=1",
   },
 ];
 
